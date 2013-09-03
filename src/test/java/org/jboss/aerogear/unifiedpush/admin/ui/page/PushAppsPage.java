2b6a9bb2bd2dce627bbf27edc3f52f0f8a4f71b5
tolis-e <tolisemm@yahoo.gr>
2013-09-03 21:14:31 +0200

    Workaround for broken build due to Travis headless GUI testing

--- src/test/java/org/jboss/aerogear/unifiedpush/admin/ui/page/PushAppsPage.java
+++ src/test/java/org/jboss/aerogear/unifiedpush/admin/ui/page/PushAppsPage.java
@@ -87,11 +87,15 @@ public class PushAppsPage extends PushServerAdminUiPage {
     }
 
     private List<WebElement> filterPushApplicationRows() {
+        try {
+            // workaround for travis headless browser testing
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            //e.printStackTrace();
+        }
         final List<WebElement> rowList = new ArrayList<WebElement>();
         if (PUSH_APPLICATION_LIST != null && PUSH_APPLICATION_LIST.size() > 0) {
-            final List<WebElement> copy = new ArrayList<WebElement>();
-            copy.addAll(PUSH_APPLICATION_LIST);
-            for (WebElement row : copy) {
+            for (WebElement row : PUSH_APPLICATION_LIST) {
                 if (row != null) {
                     final List<WebElement> cols = row.findElements(By.tagName("td"));
                     if (cols != null && cols.size() == 5) {
@@ -106,9 +110,7 @@ public class PushAppsPage extends PushServerAdminUiPage {
     public List<PushApplication> getPushAppList() {
         final List<PushApplication> pushAppList = new ArrayList<PushApplication>();
         if (PUSH_APPLICATION_LIST != null && PUSH_APPLICATION_LIST.size() > 0) {
-            final List<WebElement> copy = new ArrayList<WebElement>();
-            copy.addAll(PUSH_APPLICATION_LIST);
-            for (WebElement row : copy) {
+            for (WebElement row : PUSH_APPLICATION_LIST) {
                 if (row != null) {
                     final List<WebElement> tableDataList = row.findElements(By.tagName("td"));
                     if (tableDataList != null && tableDataList.size() == 5) {
